559da1f78c5c421741e4819cb4339ce9ea46a79a
Allan Sales da Costa Melo - PrestServ <allan.sales.prestserv@petrobras.com.br>
2026-02-04 12:24:13 -0300

    escreve os dados na pasta de dados da camada gold

--- src/gold/build_gold.py
+++ src/gold/build_gold.py
@@ -6,18 +6,29 @@ from src.utils.sla_calculation import get_business_hours_diff, get_expected_sla_
 
 config = load_config()
 
+# Read file from silver
 path_data_silver = config['paths']['data']['silver']
 silver_issue_filename = config['artifacts']['data']['silver']['issue']
-
 issues_data_silver = read_from_source(path_data_silver, silver_issue_filename)
-print(issues_data_silver.head(2))
 
+# Filter finished and valid issues
 finished_issues = issues_data_silver[issues_data_silver.status != "Open"]
 valid_issues = finished_issues[finished_issues.dates_quality == 'VALID']
 valid_issues.info()
 
+# Compute business hours to resolve sla
 valid_issues["business_hours_to_sla_resolution"] = valid_issues.apply(lambda row: get_business_hours_diff(row["created_at"], row["resolved_at"]), axis=1)
 
+# Compute expected hours to resolve sla
 valid_issues = get_expected_sla_hours_to_resolution(valid_issues, 'priority')
 
+# Check whether the sla is violated or not
 valid_issues["is_sla_violated"] = get_is_sla_violated(valid_issues, "business_hours_to_sla_resolution", "expected_sla_hours_to_resolution")
+
+# Save issues and projects in the gold data folder as parquet
+path_data_gold = config['paths']['data']['gold']
+issue_filename = config['artifacts']['data']['gold']['issue']
+project_filename = config['artifacts']['data']['gold']['project']
+
+write_to_destination(valid_issues, path_data_gold, issue_filename)
+write_to_destination(valid_issues, path_data_gold, project_filename)
